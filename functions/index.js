8827b32efe214315d8d333273031adeec5ab2d00
Edward <71886023+edward62740@users.noreply.github.com>
2022-06-03 13:50:15 +0800

    Added auto clean unused accounts function

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,80 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
+const PromisePool = require('es6-promise-pool');
+// Maximum concurrent account deletions.
+const MAX_CONCURRENT = 3;
+
+
+
+
+
+/**
+* Run once a day at midnight, to cleanup the users
+* Manually run the task here https://console.cloud.google.com/cloudscheduler
+*/
+exports.I2DSaccountCleanup = functions.pubsub.schedule('every day 00:00').onRun(async context => {
+  // Fetch all user details.
+  const inactiveUsers = await getInactiveUsers();
+  // Use a pool so that we delete maximum `MAX_CONCURRENT` users in parallel.
+  const promisePool = new PromisePool(() => deleteInactiveUser(inactiveUsers), MAX_CONCURRENT);
+  await promisePool.start();
+  functions.logger.log('User cleanup finished');
+});
+
+/**
+ * Deletes one inactive user from the list.
+ */
+function deleteInactiveUser(inactiveUsers) {
+  if (inactiveUsers.length > 0) {
+    const userToDelete = inactiveUsers.pop();
+
+    // Delete the inactive user.
+    return admin.auth().deleteUser(userToDelete.uid).then(() => {
+      return functions.logger.log(
+        'Deleted user account',
+        userToDelete.uid,
+        'because of inactivity'
+      );
+    }).catch((error) => {
+      return functions.logger.error(
+        'Deletion of inactive user account',
+        userToDelete.uid,
+        'failed:',
+        error
+      );
+    });
+  } else {
+    return null;
+  }
+}
+
+/**
+ * Returns the list of all inactive users.
+ */
+async function getInactiveUsers(users = [], nextPageToken) {
+  const result = await admin.auth().listUsers(1000, nextPageToken);
+  // Find users that have not signed in in the last 3 days.
+  const inactiveUsers = result.users.filter(
+    user => Date.parse(user.metadata.lastRefreshTime || user.metadata.lastSignInTime) < (Date.now() - 3 * 24 * 60 * 60 * 1000));
+
+  // Concat with list of previously found inactive users if there was more than 1000 users.
+  users = users.concat(inactiveUsers);
+
+  // If there are more users to fetch we fetch them.
+  if (result.pageToken) {
+    return getInactiveUsers(users, result.pageToken);
+  }
+
+  return users;
+}
+
 
 /**
  * Triggers when the state of a sensor changes.
  * Sends an alert message for S_ALERTING and a notification for other state changes.
  */
-exports.I2DSstateChangeNotification = functions.region('asia-southeast1').database.ref('/devices/{devId}/state')
+exports.I2DSstateChangeNotification = functions.region('us-central1').database.ref('/devices/{devId}/state')
   .onWrite(async (change, context) => {
     const devId = (context.params.devId).replace(/\D/g, '');
 
@@ -69,8 +137,8 @@ exports.I2DSstateChangeNotification = functions.region('asia-southeast1').databa
     let loc_string;
     loc_string = 'an unknown location';
     loc_keys.forEach((key, index) => {
-      if(key == devId)
-      loc_string = 'the ' + loc[index];
+      if (key == devId)
+        loc_string = 'the ' + loc[index];
 
     });
     const warning_payload_pirsn = {
@@ -89,7 +157,6 @@ exports.I2DSstateChangeNotification = functions.region('asia-southeast1').databa
       notification: {
         title: 'I²DS Messaging Service',
         body: '' + hw_string + ' (ID ' + devices[devId].self_id + ') has ' + state_string + '.',
-        icon: 'https://github.com/edward62740/Wireless-Mesh-Network-System/blob/master/Documentation/ltsn.png',
       }
     };
     const warning_end_pirsn_HIGH = {
@@ -109,25 +176,27 @@ exports.I2DSstateChangeNotification = functions.region('asia-southeast1').databa
     levels = Object.values(tokensSnapshot.val());
     const tokensToRemove = [];
     await Promise.all(tokens.map(async (token, index) => {
-      let response = (devices[devId].hw == 137 && devices[devId].state == 5 && devices[devId].trigd > 2) ? 
-      await admin.messaging().sendToDevice(token, warning_end_pirsn_HIGH, priority) : (devices[devId].state == 204) ?
-      ((devices[devId].hw == 137) ? 
-      await admin.messaging().sendToDevice(token, warning_payload_pirsn, priority)
-      : await admin.messaging().sendToDevice(token, warning_payload_acsn, priority))
-      : ((levels[index] == 1 || levels[index] == 3) && (devices[devId].hw != 138)) ? 
-      await admin.messaging().sendToDevice(token, info_payload, priority) 
-      : null;
-      const error = response.results[0].error;
-      if (error) {
-        functions.logger.error(
-          'Failure sending notification to',
-          token,
-          error
-        );
-        // Cleanup the tokens who are not registered anymore.
-        if (error.code === 'messaging/invalid-registration-token' ||
-          error.code === 'messaging/registration-token-not-registered') {
-          tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+      let response = (devices[devId].hw == 137 && devices[devId].state == 5 && devices[devId].trigd > 2) ?
+        await admin.messaging().sendToDevice(token, warning_end_pirsn_HIGH, priority) : (devices[devId].state == 204) ?
+          ((devices[devId].hw == 137) ?
+            await admin.messaging().sendToDevice(token, warning_payload_pirsn, priority)
+            : await admin.messaging().sendToDevice(token, warning_payload_acsn, priority))
+          : ((levels[index] == 1 || levels[index] == 3) && (devices[devId].hw != 138)) ?
+            await admin.messaging().sendToDevice(token, info_payload, priority)
+            : null;
+      if(response != null){
+        const error = response.results[0].error;
+        if (error) {
+          functions.logger.error(
+            'Failure sending notification to',
+            token,
+            error
+          );
+          // Cleanup the tokens who are not registered anymore.
+          if (error.code === 'messaging/invalid-registration-token' ||
+            error.code === 'messaging/registration-token-not-registered') {
+            tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+          }
         }
       }
     }));
@@ -138,7 +207,7 @@ exports.I2DSstateChangeNotification = functions.region('asia-southeast1').databa
  * Triggers when a new sensor is added to the database.
  * Sends a message to indicate the new device.
  */
-exports.I2DSdeviceAddedNotification = functions.region('asia-southeast1').database.ref('/devices/{devId}')
+exports.I2DSdeviceAddedNotification = functions.region('us-central1').database.ref('/devices/{devId}')
   .onCreate(async (change, context) => {
     const devId = (context.params.devId).replace(/\D/g, '');
 
@@ -208,7 +277,7 @@ exports.I2DSdeviceAddedNotification = functions.region('asia-southeast1').databa
  * Triggers when the cpn info data changes.
  * Sends a message informing the user of either a PR or security failure.
  */
-exports.I2DSinfoNotification = functions.region('asia-southeast1').database.ref('/info')
+exports.I2DSinfoNotification = functions.region('us-central1').database.ref('/info')
   .onUpdate(async (change, context) => {
 
     const getTokensPromise = admin.database()
@@ -243,6 +312,8 @@ exports.I2DSinfoNotification = functions.region('asia-southeast1').database.ref(
         }
       };
     }
+    else
+      return;
 
     // Listing all tokens as an array.
     tokens = Object.keys(tokensSnapshot.val());
@@ -269,3 +340,6 @@ exports.I2DSinfoNotification = functions.region('asia-southeast1').database.ref(
     }));
     return Promise.all(tokensToRemove);
   });
+
+
+
